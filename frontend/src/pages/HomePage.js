4a251a6451c3f173e1778296b165e710ee7c9f2f
RileySuehsschlof <riley.suehsschlof@rdpolytech.ca>
2024-11-09 15:36:32 -0700

    Added Map With directions
    Added a map, a marker for the destination, and direction pathing

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,17 +3,19 @@ import Quote from "../components/Quote";
 import About from "../components/About";
 import Services from "../components/Services";
 import Footer from "../components/Footer";
+import GoogleMap from "../components/Map";
 
 const HomePage = () => {
-    return (
-        <div>
-            <Navbar />
-            <Quote />
-            <About />
-            <Services />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Quote />
+      <About />
+      <Services />
+      <GoogleMap />
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
